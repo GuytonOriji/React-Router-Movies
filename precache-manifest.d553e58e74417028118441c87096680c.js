249b9941e5360bba555c0ef4334471e09d59c338
thisguycodez <guyton578@gmail.com>
2020-01-28 16:52:31 -0500

    Updates

---
+++ precache-manifest.d553e58e74417028118441c87096680c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3604f2c4e83277340cc963af54633e5c",
+    "url": "/React-Router-Movies/index.html"
+  },
+  {
+    "revision": "1f23af37d803f211a6d9",
+    "url": "/React-Router-Movies/static/css/main.b6bf4a22.chunk.css"
+  },
+  {
+    "revision": "0442d8384710df866ef1",
+    "url": "/React-Router-Movies/static/js/2.61bbe72d.chunk.js"
+  },
+  {
+    "revision": "1f23af37d803f211a6d9",
+    "url": "/React-Router-Movies/static/js/main.bc89600f.chunk.js"
+  },
+  {
+    "revision": "9dd66e5a9b20da2bd32e",
+    "url": "/React-Router-Movies/static/js/runtime-main.a86cb4de.js"
+  }
+]);
